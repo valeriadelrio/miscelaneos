cc622f700fd983df35effc9ab69b2ac791f3efb6
valeriadelrio <valeriadlrio@gmail.com>
2017-08-23 12:28:26 -0300

    uso de procesos asincronos con indicadores de usuario

--- src/app/components/clases/clases.component.ts
+++ src/app/components/clases/clases.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class ClasesComponent implements OnInit {
 
   alerta:string="alert-danger";
+  loading:boolean=false;
   propiedades:Object = {
     danger: false
   }
@@ -15,4 +16,8 @@ export class ClasesComponent implements OnInit {
   ngOnInit() {
   }
 
+  ejecutar(){
+    this.loading = true;
+    setTimeout(() => this.loading=false, 3000)
+  }
 }
